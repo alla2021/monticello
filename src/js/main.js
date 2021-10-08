60f2b6dc8a21d3a8c356aabf7caa93d0c7e99cc7
Alla Honcharova <alla7gon@gmail.com>
2021-10-08 23:44:57 +0300

    added responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $('.slider').slick({
+      prevArrow: $('.prev-btn'),
+      nextArrow: $('.next-btn'),
         arrows: true,
         dots: true,
         slidesToShow: 3,
