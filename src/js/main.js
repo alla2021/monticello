6d6029996ecdd71756994dc647dc84819b0b47ca
Alla <alla7gon@gmail.com>
2021-10-08 18:38:07 +0300

    1

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        infinite: false,
+        speed: 300,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    })
+})
+
