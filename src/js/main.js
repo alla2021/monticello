70fec056fcbb9d93d423a4e3379ca59f636ba754
Alla <alla7gon@gmail.com>
2021-10-09 13:42:01 +0300

    2

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,46 @@
-$(document).ready(function() {
-    $('.slider').slick({
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
 
-        arrows: true,
-        dots: true,
-        slidesToShow: 3,
-        infinite: true,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-              breakpoint: 985,
-              settings: {
-                arrows: false,
-                slidesToShow: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: false,
-                arrows: false,
-              }
-            }
-        ]
-    })
-})
-
-
+$( document ).ready(function() {
+	$('.js-burger-btn').on('click', function() {
+			if($('.js-header').hasClass('active')) {
+					$('.js-header').removeClass('active');
+			} else {
+					$('.js-header').addClass('active');
+			}
+	})
+});
 
 
 function initMap() {
   new google.maps.Map(document.getElementById("map"), {
-    mapId: '078384c4f8f2ab',
-    center: { lat: 48.85, lng: 2.35 },
+    center: {lat: 48.43306439179746, lng: 34.84722679687054},
     zoom: 12,
+		mapId: '3078384c4f8f2ab9',
   });
+}
+
 
 
