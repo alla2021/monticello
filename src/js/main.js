ff5f277c991145583fac4489dab997b761b57da3
Alla <alla7gon@gmail.com>
2021-10-09 10:29:58 +0300

    gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,19 @@
 $(document).ready(function() {
     $('.slider').slick({
-      prevArrow: $('.prev-btn'),
-      nextArrow: $('.next-btn'),
+
         arrows: true,
         dots: true,
         slidesToShow: 3,
-        infinite: false,
-        speed: 300,
+        infinite: true,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 985,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: false,
                 arrows: false,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                autoplay: true,
-                autoplaySpeed: 2000,
-                dots: false,
-                arrows: false,
+                slidesToShow: 2
               }
             },
             {
@@ -44,6 +28,7 @@ $(document).ready(function() {
         ]
     })
 })
+
 
 
 
