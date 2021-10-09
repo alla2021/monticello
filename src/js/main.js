54c8ca43e65490f8acdb4e38cdb1f58b8c71ee3d
Alla <alla7gon@gmail.com>
2021-10-09 14:41:58 +0300

    scroll

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,18 @@ $(document).ready(function(){
 	});
 });
 
-
+$(document).ready(function () {
+	$("#scroll, #header__nav").on("click", "a", function (event) {
+					event.preventDefault();
+					let id = $(this).attr('href'),
+							top = $(id).offset().top;
+					$('body,html').animate({
+									scrollTop: top
+							}
+							, 1000);
+			}
+	);
+});
 
 
 
