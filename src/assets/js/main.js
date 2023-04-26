9944350195bfe382b071fe360b658775132b4d7f
Alla Honcharova <alla7gon@gmail.com>
2023-04-26 17:44:03 +0300

    fixed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,9 +3,9 @@ var swiper1 = new Swiper('.swiper1', {
 		spaceBetween: 30,
 		loop: true,
 		autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
+			delay: 3000,
+			disableOnInteraction: false,
+		},
 });
 
 
